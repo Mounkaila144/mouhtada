eb4ce2b5b93f1ba1043e5c8ef53ec7be25dff2b5
mounkaila <mounkaila144@gmail.com>
2022-12-26 10:21:24 +0100

    test

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'mouhtada.allcine227.com',
-        pathname: '/storage/**'
-      }
-    ]
+  images:{
+    loader: "akamai",
+    path: "/",
+    protocol: 'https',
+    hostname: 'mouhtada.allcine227.com',
+    pathname: '/storage/**'
   }
-
 }
+
+module.exports = nextConfig
