36acf6259ef61d4eee987686b651ea7973c755f4
mounkaila <mounkaila144@gmail.com>
2022-12-26 10:14:20 +0100

    test

--- next.config.js
+++ next.config.js
@@ -7,11 +7,11 @@ module.exports = {
   images: {
     remotePatterns: [
       {
-        formats: ['image/png'],
         protocol: 'https',
         hostname: 'mouhtada.allcine227.com',
-      },
-    ],
-  },
+        pathname: '/storage/**'
+      }
+    ]
+  }
 
 }
