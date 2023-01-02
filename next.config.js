1871f9db5901ae4e24a7479b0ee46a3158b0af03
mounkaila <mounkaila144@gmail.com>
2023-01-02 20:43:30 +0100

    edit 4

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+}
+
+module.exports = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '8000',
+        protocol: 'https',
+        hostname: 'mouhtada.allcine227.com',
         pathname: '/storage/**',
       },
     ],
   },
-}
 
-module.exports = nextConfig
+}
